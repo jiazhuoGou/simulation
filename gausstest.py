da9746acb86dd3c598240023aceb882f67a632d9
jiazhuoGou <goujz@qq.com>
2023-03-01 21:37:53 +0800

    31

---
+++ gausstest.py
@@ -0,0 +1,11 @@
+'''
+Author: jiazhuoGou goujz@qq.com
+Date: 2023-03-01 16:38:39
+LastEditors: jiazhuoGou goujz@qq.com
+LastEditTime: 2023-03-01 16:50:17
+FilePath: \clustring\gausstest.py
+Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+'''
+
+
+
