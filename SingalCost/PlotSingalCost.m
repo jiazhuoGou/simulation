7a8a915ecdb383b95a1c5ac0a14a4ade20f57daa
jiazhuoGou <goujz@qq.com>
2023-04-07 16:38:30 +0800

    2023/4/7/1638

---
+++ SingalCost/PlotSingalCost.m
@@ -0,0 +1,22 @@
+%% 横坐标： 无人机终端数量
+user_num             =  [5;10;15;20];
+
+%% 纵坐标： 各类算法的信令开销
+SingalCost_BLQoE  = readmatrix('D:\simulation\SingalCost\SingalCost.xlsx','Sheet','SingalCost', 'Range','1:1');
+SingalCost_AHPSAW = readmatrix('D:\simulation\SingalCost\SingalCosts.xlsx','Sheet','SingalCost', 'Range','2:2');
+SingalCost_GRA = readmatrix('D:\simulation\SingalCost\SingalCosts.xlsx','Sheet','SingalCost', 'Range','3:3');
+SingalCost_RSR = readmatrix('D:\simulation\SingalCost\SingalCosts.xlsx','Sheet','SingalCost', 'Range','4:4');
+
+plot(user_num,SingalCost_BLQoE, '--or','LineWidth',1,'DisplayName','\fontname{Times New Roman}BLQoE');hold on;
+plot(user_num,SingalCost_AHPSAW, '--sb','LineWidth',1,'DisplayName','\fontname{Times New Roman}AHPSAW'); hold on;
+plot(user_num,SingalCost_GRA, '--*g','LineWidth',1,'DisplayName','\fontname{Times New Roman}GRA'); hold on;
+plot(user_num,SingalCost_RSR,  '--^m','LineWidth',1,'DisplayName','\fontname{Times New Roman}RSR'); hold on;
+
+% grid on;
+xlabel('\fontname{宋体}无人机终端数量(台)','FontSize',14);
+ylabel('\fontname{宋体}吞吐量\fontname{Times New Roman}(Mbps)','FontSize',14);
+hold off;
+lgd = legend;
+lgd.NumColumns = 1;
+lgd.Location = 'NorthWest' ;
+set(lgd ,'box','off')
