dec63d19295061ba6dd341a44e941e9956b6f8ce
jiazhuoGou <goujz@qq.com>
2023-04-07 16:35:38 +0800

    2023/4/7/1635

--- Throughoutput/PlotThroughput.m
+++ Throughoutput/PlotThroughput.m
@@ -2,15 +2,15 @@
 user_num             =  [5;10;15;20];
 
 %% 纵坐标： 各类算法的吞吐量
-time_cost_BLQoE  = readmatrix('D:\simulation\Throughoutput\Throughput.xlsx','Sheet','Timecost', 'Range','1:1');
-time_cost_AHPSAW = readmatrix('D:\simulation\Throughoutput\Throughput.xlsx','Sheet','Timecost', 'Range','2:2');
-time_cost_GRA = readmatrix('D:\simulation\Throughoutput\Throughput.xlsx','Sheet','Timecost', 'Range','3:3');
-time_cost_M = readmatrix('D:\simulation\Throughoutput\Throughput.xlsx','Sheet','Timecost', 'Range','4:4');
+Throughput_BLQoE  = readmatrix('D:\simulation\Throughoutput\Throughput.xlsx','Sheet','Throughput', 'Range','1:1');
+Throughput_AHPSAW = readmatrix('D:\simulation\Throughoutput\Throughput.xlsx','Sheet','Throughput', 'Range','2:2');
+Throughput_GRA = readmatrix('D:\simulation\Throughoutput\Throughput.xlsx','Sheet','Throughput', 'Range','3:3');
+Throughput_RSR = readmatrix('D:\simulation\Throughoutput\Throughput.xlsx','Sheet','Throughput', 'Range','4:4');
 
-plot(user_num,time_cost_BLQoE, '--or','LineWidth',1,'DisplayName','\fontname{Times New Roman}BLQoE');hold on;
-plot(user_num,time_cost_AHPSAW, '--sb','LineWidth',1,'DisplayName','\fontname{Times New Roman}AHPSAW'); hold on;
-plot(user_num,time_cost_GRA, '--*g','LineWidth',1,'DisplayName','\fontname{Times New Roman}GRA'); hold on;
-plot(user_num,time_cost_M,  '--^m','LineWidth',1,'DisplayName','\fontname{Times New Roman}M'); hold on;
+plot(user_num,Throughput_BLQoE, '--or','LineWidth',1,'DisplayName','\fontname{Times New Roman}BLQoE');hold on;
+plot(user_num,Throughput_AHPSAW, '--sb','LineWidth',1,'DisplayName','\fontname{Times New Roman}AHPSAW'); hold on;
+plot(user_num,Throughput_GRA, '--*g','LineWidth',1,'DisplayName','\fontname{Times New Roman}GRA'); hold on;
+plot(user_num,Throughput_RSR,  '--^m','LineWidth',1,'DisplayName','\fontname{Times New Roman}RSR'); hold on;
 
 % grid on;
 xlabel('\fontname{宋体}无人机终端数量(台)','FontSize',14);
