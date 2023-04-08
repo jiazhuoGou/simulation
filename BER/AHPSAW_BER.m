28384a6527659555a6f5aac9c1b922c671a1022e
jiazhuoGou <goujz@qq.com>
2023-04-08 15:55:34 +0800

    2023/4/8/1555

---
+++ BER/AHPSAW_BER.m
@@ -0,0 +1,83 @@
+function [ber_average] = AHPSAW_BER(selected_numbers)
+%AHPSAW_BER 此处显示有关此函数的摘要
+%   此处显示详细说明
+UAV = readmatrix('D:\simulation\data\InfoUAV.xlsx','Sheet','InfoUAVSheet');
+BS = readmatrix('D:\simulation\data\InfoBs.xlsx','Sheet','InfoBsSheet');
+DATA = readmatrix('D:\simulation\data\InfoData.xlsx','Sheet','InfoDataSheet');
+
+%% 数据
+[uav_rows, ~] = size(UAV);
+ber_average = 0;
+
+
+%% 具体流程
+for i = 101 : uav_rows + 100
+    uav = UAV(i-100, :); % i是无人机编号
+    if ~ismember(selected_numbers, uav(8)) % 如果该无人机不在，那么不用回传
+        continue;
+    end
+    candiate_net = SimpleAdditiveWeighting_CalcCanNet(uav);
+    target_net = SimpleAdditiveWeighting(candiate_net);
+    for j = size(target_net,1)
+        ap = target_net(j,:);
+        ap_id = ap(1);
+        if ap_id < 100 % 基站
+            if  DATA(i-100, 3) == 1 &&  BS(ap_id, 7) >= 1 % 说明这个基站可以接小数据
+                BS(ap_id, 7) = BS(ap_id,7) - 1; % 更新资源快
+                best_net = target_net(j,:); % 第j个就是最优的
+                % 计算误码率
+                ber_average = ber_average + CalcBer(uav, ap_id, UAV, BS);
+                disp(['无人机 ', num2str(i),'  接入点 : ', num2str(ap_id)]);
+                flag = true;
+                break;
+            elseif DATA(i-100, 3) == 2 && BS(ap_id, 7) >= 3 % 说明可以接大数据
+                BS(ap_id, 7) = BS(ap_id,7) - 3; % 更新资源快
+                best_net = target_net(j,:); % 第j个就是最优的
+                % 计算误码率
+                ber_average = ber_average + CalcBer(uav, ap_id, UAV, BS);
+                disp(['无人机 ', num2str(i),'  接入点 : ', num2str(ap_id)]);
+                flag = true;
+                break;
+            end
+        else % 接入点是无人机
+            if DATA(i-100, 3) == 1 && UAV(ap_id - 100, 5) >= 1
+                UAV(ap_id - 100, 5) = UAV(ap_id - 100,5) - 1;
+                best_net = target_net(j,:);
+                % 计算误码率
+                ber_average = ber_average + CalcBer(uav, ap_id, UAV, BS);
+                disp(['无人机 ', num2str(i),'  接入点 : ', num2str(ap_id)]);
+                flag = true;
+                break;
+            elseif DATA(i-100, 3) == 2 && UAV(ap_id - 100, 5) >= 3
+                UAV(ap_id - 100, 5) = UAV(ap_id - 100,5) - 3;
+                best_net = target_net(j,:);
+                % 计算误码率
+                ber_average = ber_average + CalcBer(uav, ap_id, UAV, BS);
+                disp(['无人机 ', num2str(i),'  接入点 : ', num2str(ap_id)]);
+                flag = true;
+                break;
+            end
+        end
+    end
+    % 如果上面都不满足
+    if ~flag
+        target_net = sortrows(candiate_net, 7, "descend");
+        best_net  = target_net(1);
+        ap_id = best_net(1);
+        if (best_net(1) < 100)
+            if  DATA(i-100, 3) == 1
+                BS(best_net(1), 7) = BS(best_net(1),7) - 1; % 更新资源快
+                % 计算误码率
+                ber_average = ber_average + CalcBer(uav, ap_id, UAV, BS);
+            else
+                BS(best_net(1), 7) = BS(best_net(1),7) - 3;
+                % 计算误码率
+                ber_average = ber_average + CalcBer(uav, ap_id, UAV, BS);
+            end
+        disp(['无人机 ', num2str(i),'  接入点 : ', num2str(best_net(1))]);
+        end
+    end
+end % for循环end
+ber_average = ber_average / size(selected_numbers,1);
+end
+
---
+++ BER/PlotBER.m
@@ -0,0 +1,22 @@
+%% 横坐标： 无人机终端数量
+user_num             =  [5;10;15;20];
+
+%% 纵坐标： 各类算法的基站负载程度
+BSLoadDegree_BLQoE  = readmatrix('D:\simulation\BER\BER.xlsx','sheet','BER','Range','1:1');
+BSLoadDegree_AHPSAW = readmatrix('D:\simulation\BER\BER.xlsx','Sheet','BER','Range','2:2');
+BSLoadDegree_GRA = readmatrix('D:\simulation\BER\BER.xlsx','Sheet','BER', 'Range','3:3');
+BSLoadDegree_RSR = readmatrix('D:\simulation\BER\BER.xlsx','Sheet','BER', 'Range','4:4');
+
+plot(user_num,BSLoadDegree_BLQoE, '--or','LineWidth',1,'DisplayName','\fontname{Times New Roman}BLQoE');hold on;
+plot(user_num,BSLoadDegree_AHPSAW, '--sb','LineWidth',1,'DisplayName','\fontname{Times New Roman}AHPSAW'); hold on;
+plot(user_num,BSLoadDegree_GRA, '--*g','LineWidth',1,'DisplayName','\fontname{Times New Roman}GRA'); hold on;
+plot(user_num,BSLoadDegree_RSR,  '--^m','LineWidth',1,'DisplayName','\fontname{Times New Roman}RSR'); hold on;
+
+% grid on;
+xlabel('\fontname{宋体}无人机终端数量(台)','FontSize',14);
+ylabel('\fontname{宋体}吞吐量\fontname{Times New Roman}(Mbps)','FontSize',14);
+hold off;
+lgd = legend;
+lgd.NumColumns = 1;
+lgd.Location = 'NorthWest' ;
+set(lgd ,'box','off')
