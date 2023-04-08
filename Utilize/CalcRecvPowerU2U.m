f7583a592747237e1a87e6c9efe20e569f41af8d
jiazhuoGou <goujz@qq.com>
2023-04-08 14:56:17 +0800

    2023/4/8/1456

--- Utilize/CalcRecvPowerU2U.m
+++ Utilize/CalcRecvPowerU2U.m
@@ -1,16 +1,13 @@
 function [ RecvPowerU2U ] = CalcRecvPowerU2U( uav1, uav2 )
-%CALCRECVPOWERU2U 此处显示有关此函数的摘要 被废弃
+%CALCRECVPOWERU2U 此处显示有关此函数的摘要 
 %   此处显示详细说明
     PU = uav1(4) ;    % 无人机发射功率dBm
-    PU = power(10, PU / 10);
-    
-    G = -31.5;
-    G = power(10, G / 10);
-    
     dis = CalcDis(uav1, uav2);
-    alpha = 2;
-    
-    RecvPowerU2U = PU * G * power(dis, -alpha) ; % 这是mw，再转换为dBm
-    RecvPowerU2U = 10 * log10(RecvPowerU2U);
+
+
+    PL_d0 = 46.4;
+    PL =PL_d0 + 10 * alpha * log10(dis);
+    small_fading = abs(normrnd(5,sqrt(1)));
+    RecvPowerU2U = PU - PL - small_fading;
 end
 
