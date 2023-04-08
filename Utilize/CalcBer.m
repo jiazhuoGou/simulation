450a31d340a6811574d5a2142d49ef24d7cad8d2
jiazhuoGou <goujz@qq.com>
2023-04-08 15:56:28 +0800

    2023/4/8/1556

--- Utilize/CalcBer.m
+++ Utilize/CalcBer.m
@@ -1,10 +1,10 @@
 function [ber] = CalcBer(uav, apid, UAV, BS)
     if apid < 100 % 基站
-        rss = CalcRecvPowerU2B(uav, BS(apid));
+        rss = CalcRecvPowerU2B(uav, BS(apid, :));
     else % 无人机
-        rss = CalcRecvPowerU2U(uav, UAV(apid-100));
+        rss = CalcRecvPowerU2U(uav, UAV(apid-100, :));
     end
-    ber = (1/sqrt(2*pi))*(-(2^(1/2)*pi^(1/2)*(erf((2^(1/2)*sqrt(rss+130+normrnd(0,10)))/2) - 1))/2);
+    ber = (1/sqrt(2*pi))*(-(2^(1/2)*pi^(1/2)*(erf((2^(1/2)*sqrt(rss+130+normrnd(0,10)))/2) - 1))/2) * 1e3;
 
 end
 
--- Utilize/CalcRecvPowerU2U.m
+++ Utilize/CalcRecvPowerU2U.m
@@ -3,7 +3,7 @@ function [ RecvPowerU2U ] = CalcRecvPowerU2U( uav1, uav2 )
 %   此处显示详细说明
     PU = uav1(4) ;    % 无人机发射功率dBm
     dis = CalcDis(uav1, uav2);
-
+    alpha = 2.03;
 
     PL_d0 = 46.4;
     PL =PL_d0 + 10 * alpha * log10(dis);
